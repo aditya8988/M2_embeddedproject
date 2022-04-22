3d6506ad32a719fdbc1a66f594c9e8b4f999dd08
aditya8988 <101787412+aditya8988@users.noreply.github.com>
2022-04-22 12:23:36 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -5,9 +5,7 @@
  * @author anoop
  * @brief activity3
  * @version 0.1
- * @date 2021-04-27
- * 
- * @copyright Copyright (c) 2021
+ * @date 2022-04-20
  * 
  */
 #define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
